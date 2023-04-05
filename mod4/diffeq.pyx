0faa2c345c83b7e43ced33a8159593f735e5b54f
djanloo <zzibecu@gmail.com>
2023-04-05 12:58:18 +0200

    burgers' does not work. didn't think too much

--- mod4/diffeq.pyx
+++ mod4/diffeq.pyx
@@ -1,6 +1,4 @@
 from cython.parallel import prange
-from libc.math cimport sqrt, log
-from libc.stdlib cimport rand, srand
 import numpy as np
 cimport cython
 
@@ -51,7 +49,7 @@ cpdef LAX(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, int n_steps
 
 cpdef LAX_WENDROFF(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, int n_steps = 10):
   """Implements a Lax-Wendroff integrator 
-  on a flux conservative PDEs
+  on a flux conservative PDE
   """
 
   print(f"Conditioning number is {v*dt/dx}")
@@ -66,16 +64,57 @@ cpdef LAX_WENDROFF(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, in
   for _ in range(n_steps):
 
     # LAX STEP
+    
     for i in range(N):
       left = u[N-1] if i == 0 else u[i-1]
       right = u[0] if i == N-1 else u[i+1]
       u_intermediate[i] = 0.5*(right+left) - v*dt/dx/2 * (right - left) 
-    
+  
     # LEAPFROG STEP
     for i in range(N):
       left = u_intermediate[N-1] if i == 0 else u_intermediate[i-1]
       right = u_intermediate[0] if i == N-1 else u_intermediate[i+1]
       u_next_time[i] = u[i] - v*dt/dx * (right - left) 
+
+    u = u_next_time.copy()
+  
+  return np.array(u)
+
+
+cpdef preburgers(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, int n_steps = 10):
+  """Solves Burgers' equation with zero diffusion:
+
+  du/dt + u du/dx = 0
+
+  Uses a Lax-Wendroff startegy.
+
+
+  """
+
+  print(f"Conditioning number is {v*dt/dx}")
+  cdef int N = len(u0)
+  cdef double [:] u = u0.copy()
+  cdef double [:] u_next_time = u0.copy()
+  cdef double [:] u_intermediate = u0.copy()
+
+  cdef int i, _
+  cdef double left, right
+
+  for _ in range(n_steps):
+
+    # LAX STEP
+    
+    for i in range(N):
+      left = 0.0 if i == 0 else u[i-1]
+      right = 0.0 if i == N-1 else u[i+1]
+      # instead of v, the propagation velocity is the mean function value
+      u_intermediate[i] = 0.5*(right+left) - (right+left) * dt/dx/2 * (right - left) 
+  
+    # LEAPFROG STEP
+    for i in range(N):
+      left = 0.0 if i == 0 else u_intermediate[i-1]
+      right = 0.0 if i == N-1 else u_intermediate[i+1]
+      u_next_time[i] = u[i] - (right+left) * dt/dx * (right - left) 
 
     u = u_next_time.copy()
   
--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from mod4.diffeq import FTCS, LAX, LAX_WENDROFF
+from mod4.diffeq import FTCS, LAX, LAX_WENDROFF, preburgers
 
 x = np.linspace(-10, 10, 300)
-u0 = np.exp(-(x/0.5)**2)
+u0 = np.exp(-(x/0.5)**2)+5
 v = 30
-intgration_args = dict(dx=x[1]-x[0], dt=0.000001, v=v, n_steps=1_000)
+intgration_args = dict(dx=x[1]-x[0], dt=0.0001, v=v, n_steps=1)
 
 
 plt.plot(u0)
 
 u = u0.copy()
 for i in range(4):
-    u = LAX_WENDROFF(u, **intgration_args)
+    u = preburgers(u, **intgration_args)
     plt.plot(u)
 
 plt.show()
