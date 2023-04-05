05472da8cd15159380a000f385272ae97e2ae747
djanloo <zzibecu@gmail.com>
2023-04-05 17:59:09 +0200

    minor

--- mod4/diffeq.pyx
+++ mod4/diffeq.pyx
@@ -64,7 +64,6 @@ cpdef LAX_WENDROFF(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, in
   for _ in range(n_steps):
 
     # LAX STEP
-    
     for i in range(N):
       left = u[N-1] if i == 0 else u[i-1]
       right = u[0] if i == N-1 else u[i+1]
@@ -82,40 +81,39 @@ cpdef LAX_WENDROFF(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, in
 
 
 cpdef preburgers(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, int n_steps = 10):
-  """Solves Burgers' equation with zero diffusion:
+  """
+  Burgers solved with Hopf-Cole tranformation.
 
-  du/dt + u du/dx = 0
+  Basically solves:
+      
+      du/dt = v d**2 u /dx**2      (Heat equation)
 
-  Uses a Lax-Wendroff startegy.
+  with a dumb integration:
 
-
+      u_next_time(i) = u(i) + r [ u(i-1) - 2 u(i) + u(i+1)  ]
   """
 
   print(f"Conditioning number is {v*dt/dx}")
   cdef int N = len(u0)
   cdef double [:] u = u0.copy()
   cdef double [:] u_next_time = u0.copy()
-  cdef double [:] u_intermediate = u0.copy()
 
   cdef int i, _
   cdef double left, right
 
   for _ in range(n_steps):
 
-    # LAX STEP
-    
     for i in range(N):
-      left = 0.0 if i == 0 else u[i-1]
-      right = 0.0 if i == N-1 else u[i+1]
-      # instead of v, the propagation velocity is the mean function value
-      u_intermediate[i] = 0.5*(right+left) - (right+left) * dt/dx/2 * (right - left) 
+      if i == 0:
+        u_next_time[i] = u[i] + 2*r*(u[i+1] - u[i-1])
+      elif i == N-1:
+        u_next_time[i] = u[i] + 2*r*(u[i-1] - u[i])
+      else:
+        u_next_time[i] = u[i] + r*(u[i-1] - 2*u[i] + u[i+1])
   
-    # LEAPFROG STEP
-    for i in range(N):
-      left = 0.0 if i == 0 else u_intermediate[i-1]
-      right = 0.0 if i == N-1 else u_intermediate[i+1]
-      u_next_time[i] = u[i] - (right+left) * dt/dx * (right - left) 
-
     u = u_next_time.copy()
+
+
   
+
   return np.array(u)
