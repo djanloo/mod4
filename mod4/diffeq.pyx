83e8930015ef2de9ab7d971672ebfbe1b080e93f
djanloo <zzibecu@gmail.com>
2023-04-06 15:03:57 +0200

    working example for propagation with LAX

--- mod4/diffeq.pyx
+++ mod4/diffeq.pyx
@@ -34,14 +34,15 @@ cpdef LAX(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, int n_steps
   cdef double [:] u_next_time = u0.copy()
 
   cdef int i, _
-  cdef double left, right
+  cdef double left, right, alpha = v*dt/dx
 
   for _ in range(n_steps):
     for i in range(N):
-      left = 0 if i == 0 else u[i-1]
-      right = 0 if i == N-1 else u[i+1]
+      # PBC
+      left  = u[i-1] if i != 0   else u[N-1]
+      right = u[i+1] if i != N-1 else u[0]
 
-      u_next_time[i] = 0.5*(right+left) - v*dt/dx/2 * (right - left) 
+      u_next_time[i] = 0.5*(right*(1-alpha) + left*(1+alpha))
     
     u = u_next_time.copy()
   
@@ -59,20 +60,24 @@ cpdef LAX_WENDROFF(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, in
   cdef double [:] u_intermediate = u0.copy()
 
   cdef int i, _
-  cdef double left, right
+  cdef double left, right, alpha = v*dt/dx
 
   for _ in range(n_steps):
 
     # LAX STEP
     for i in range(N):
-      left = u[N-1] if i == 0 else u[i-1]
-      right = u[0] if i == N-1 else u[i+1]
-      u_intermediate[i] = 0.5*(right+left) - v*dt/dx/2 * (right - left) 
+      # PBC
+      left  = u[i-1] if i != 0   else u[N-1]
+      right = u[i+1] if i != N-1 else u[0]
+
+      u_intermediate[i] = 0.5*(right*(1-alpha) + left*(1+alpha))
   
     # LEAPFROG STEP
     for i in range(N):
-      left = u_intermediate[N-1] if i == 0 else u_intermediate[i-1]
-      right = u_intermediate[0] if i == N-1 else u_intermediate[i+1]
+      # PBC
+      left = u_intermediate[i-1] if i != 0 else u_intermediate[N-1]
+      right = u_intermediate[i+1] if i != N-1 else u_intermediate[0]
+
       u_next_time[i] = u[i] - v*dt/dx * (right - left) 
 
     u = u_next_time.copy()
--- test.py
+++ test.py
@@ -1,19 +1,39 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
+import seaborn as sns;
 from mod4.diffeq import FTCS, LAX, LAX_WENDROFF, preburgers
 
-x = np.linspace(-10, 10, 300)
-u0 = np.exp(-(x/0.5)**2)+5
-v = 30
-intgration_args = dict(dx=x[1]-x[0], dt=0.0001, v=v, n_steps=1)
+N = 2000
+Ntimes = 10
+x = np.linspace(0, 1, N, endpoint=False)
 
+# u0 = np.zeros(N)
+# for i in range(1,10):
+#     u0 += np.sin(i*3*2*np.pi*x)
 
-plt.plot(u0)
+u0 = np.exp(-(x-0.5)**2/0.025)
 
+v = 1.0
+dx = x[1]-x[0]
+
+integration_args = dict(dx=dx, dt=1e-4, v=v, n_steps=1e5)
+
+fig, (axx, axk) = plt.subplots(2,1)
+colors = sns.color_palette("flare", Ntimes)
 u = u0.copy()
-for i in range(4):
-    u = preburgers(u, **intgration_args)
-    plt.plot(u)
+for i in range(Ntimes):
+
+    axx.plot(x, u, color=colors[i])
+
+    g = np.fft.fft(u)
+
+    k = np.fft.fftfreq(N, dx)
+
+    axk.step(k[:N//2], np.abs(g[:N//2]), where="mid", color=colors[i])
+    
+    u = LAX_WENDROFF(u, **integration_args)
+
+axk.set_xscale('log')
+axk.set_yscale('log')
 
 plt.show()
