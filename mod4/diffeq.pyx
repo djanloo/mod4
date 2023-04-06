2cf5b3250192dd13d3987d278dc863fb34c26d1c
djanloo <zzibecu@gmail.com>
2023-04-06 16:07:18 +0200

    kinda working burgers

--- mod4/diffeq.pyx
+++ mod4/diffeq.pyx
@@ -84,4 +84,67 @@ cpdef LAX_WENDROFF(double [:] u0, double v=0.5, double dx=0.1, double dt=0.1, in
   
   return np.array(u)
 
+def burgers_lw(double [:] u0, double dx=0.1, double dt=0.1, int n_steps = 10):
+  print(f"alpha {dt/dx}")
+  cdef int N = len(u0)
+  cdef double [:] u = u0.copy()
+  cdef double [:] u_next_time = u0.copy()
+  cdef double [:] u_intermediate = u0.copy()
 
+  cdef int i, _
+  cdef double left, right, alpha = dt/dx
+
+  for _ in range(n_steps):
+
+    # LAX STEP
+    for i in range(N):
+      # PBC
+      left  = u[i-1] if i != 0   else u[N-1]
+      right = u[i+1] if i != N-1 else u[0]
+
+      u_intermediate[i] = 0.5*(left + right) - alpha*0.5*(left+right)*0.5*(right-left)
+  
+    # LEAPFROG STEP
+    for i in range(N):
+      # PBC
+      left = u_intermediate[i-1] if i != 0 else u_intermediate[N-1]
+      right = u_intermediate[i+1] if i != N-1 else u_intermediate[0]
+
+      u_next_time[i] = u[i] -  0.5*(left+right) *   alpha*(right - left)
+
+    u = u_next_time.copy()
+  
+  return np.array(u)
+
+
+def burgers_lw_forcing_extremum(double [:] u0, double [:] f_t, double dx=0.1, double dt=0.1, int n_steps = 10):
+  print(f"alpha {dt/dx}")
+  cdef int N = len(u0)
+  cdef double [:] u = u0.copy()
+  cdef double [:] u_next_time = u0.copy()
+  cdef double [:] u_intermediate = u0.copy()
+
+  cdef int i, _
+  cdef double left, right, alpha = dt/dx
+
+  for _ in range(n_steps):
+
+    # LAX STEP
+    for i in range(N):
+      # PBC
+      left  = u[i-1] if i != 0   else f_t(_)
+      right = u[i+1] if i != N-1 else u[0]
+
+      u_intermediate[i] = 0.5*(left + right) - alpha*0.5*(left+right)*0.5*(right-left)
+  
+    # LEAPFROG STEP
+    for i in range(N):
+      # PBC
+      left = u_intermediate[i-1] if i != 0 else u_intermediate[N-1]
+      right = u_intermediate[i+1] if i != N-1 else u_intermediate[0]
+
+      u_next_time[i] = u[i] -  0.5*(left+right) *   alpha*(right - left)
+
+    u = u_next_time.copy()
+  
+  return np.array(u)
--- test.py
+++ test.py
@@ -1,22 +1,26 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns;
-from mod4.diffeq import FTCS, LAX, LAX_WENDROFF
+from mod4.diffeq import FTCS, LAX, LAX_WENDROFF, burgers_lw
 
-N = 2000
+N = 5000
 Ntimes = 10
 x = np.linspace(0, 1, N, endpoint=False)
 
 # u0 = np.zeros(N)
-# for i in range(1,10):
+# for i in range(1,50):
 #     u0 += np.sin(i*3*2*np.pi*x)
 
-u0 = np.exp(-(x-0.5)**2/0.025)
+# u0 = np.exp(-(x-0.5)**2/0.025)
 
-v = 1.0
+# u0 = np.exp(-((x - 0.2)/0.05)**60)
+
+
+u0 = 0.07*np.sin(100*2*np.pi*x)*np.exp(-((x-0.3)/0.02)**2) + 13.7
+
 dx = x[1]-x[0]
 
-integration_args = dict(dx=dx, dt=1e-4, v=v, n_steps=1e5)
+integration_args = dict(dx=dx, dt=1e-6, n_steps=5e3)
 
 fig, (axx, axk) = plt.subplots(2,1)
 colors = sns.color_palette("flare", Ntimes)
@@ -29,9 +33,9 @@ for i in range(Ntimes):
 
     k = np.fft.fftfreq(N, dx)
 
-    axk.step(k[:N//2], np.abs(g[:N//2]), where="mid", color=colors[i])
+    axk.step(k[:N//2], np.abs(g[:N//2])**2, where="mid", color=colors[i])
     
-    u = LAX_WENDROFF(u, **integration_args)
+    u = burgers_lw(u, **integration_args)
 
 axk.set_xscale('log')
 axk.set_yscale('log')
