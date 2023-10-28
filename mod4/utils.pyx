95094b3493c3ca27d2ed9e8307c1ca9f1fa0ca89
djanloo <zzibecu@gmail.com>
2023-10-28 12:47:51 +0200

    switched to complex analytical for underdamped

--- mod4/utils.pyx
+++ mod4/utils.pyx
@@ -59,7 +59,7 @@ cpdef cyclic_tridiag(double [:] lower, double [:] diag, double [:] upper, double
     return np.array( x0 - np.array(delta)/(1 + v[0]*q[0] + v[N-1]*q[N-1]))
 
 
-cdef double [:,:] stupid_inverse(double [:,:] A):
+cdef complex [:,:] stupid_inverse(complex [:,:] A):
     det = np.linalg.det(A)
     B = A.copy()
     B[0, 0] = A[1,1]/det 
@@ -67,40 +67,74 @@ cdef double [:,:] stupid_inverse(double [:,:] A):
     B[0,1] = -A[1,0]/det 
     B[1,0] = -A[0,1]/det
     return B
+    
 
-cpdef double [:,:] analytic(double [:, :] X, double [:,:] V, double time, 
+cpdef complex [:,:] analytic(double [:, :] X, double [:,:] V, double time, 
               double x0, double v0, 
               physical_params):
+    cdef unsigned int N, M, i, j
+    cdef double omega_squared, gamma, sigma
+    cdef complex [:,:] result, G, S
 
     N, M = X.shape[0], X.shape[1]
     omega_squared, gamma, sigma = map(physical_params.get, ['omega_squared', 'gamma', 'sigma'])
-
-    l1, l2 = 0.5 * gamma + np.sqrt( (0.5*gamma)**2 - omega_squared**2), 0.5 * gamma - np.sqrt( (0.5*gamma)**2 - omega_squared**2)
+    under_root = (0.5*gamma)**2 - omega_squared**2
+    if under_root < 0:
+        l1, l2 = 0.5*gamma + 1j*np.sqrt(-under_root),  0.5*gamma - 1j*np.sqrt(-under_root)
+    else:
+        l1, l2 = 0.5 * gamma + np.sqrt(under_root), 0.5 * gamma - np.sqrt(under_root)
     exp1, exp2 = np.exp(-l1*time), np.exp(-l2*time)
-    G = [[(l1*exp1 - l2*exp2)/(l1 - l2), (exp2 - exp1)/(l1 -l2)],
-         [omega_squared**2*(exp1 - exp2)/(l1-l2), (l1*exp1 - l2*exp2)/(l1 -l2)]]
-    G = np.array(G)
+    # print(f"Determinant of gamma matrix: {omega_squared:.1f}")
+    G = np.array([[(l1*exp2 - l2*exp1)/(l1 - l2), (exp2 - exp1)/(l1 -l2)],
+         [omega_squared*(exp1 - exp2)/(l1-l2), (l1*exp1 - l2*exp2)/(l1 -l2)]], dtype=complex)
 
-    Sigm11 = sigma**2/(l1 -l2)**2*( (l1 + l2)/l1/l2 - 4*(1- exp1*exp2)/(l1+l1)- exp1**2 - exp2**2)
-    Sigm12 = sigma**2/(l1 + l2)**2*(exp1 - exp2)**2
-    Sigm22 = sigma**2/(l1-l2)**2*(l1+l2 + 4*l1*l2/(l1+l2)*(exp1*exp2 - 1) -l1*exp1**2 -l2*exp2**2)
+    # print("G", np.array(G))
+    detG=G[0,0]*G[1,1] - G[1,0]*G[0,1]
+    # print(f"detG = {detG} --- exp(det -gamma t) = {np.exp(-omega_squared*time)} ")
+
+    Sigm11 = sigma**2/gamma/(gamma**2 - 4*omega_squared)*( gamma/omega_squared - 4*(1- exp1*exp2)/gamma - exp1**2/l1 - exp2**2/l2)
+    Sigm12 = sigma**2/gamma/(gamma**2 - 4*omega_squared)*(exp1 - exp2)**2
+    Sigm22 = sigma**2/gamma/(gamma**2 - 4*omega_squared)*( gamma + 4*omega_squared/gamma*(exp1*exp2 - 1) - l1*exp1**2 -l2*exp2**2)
 
     S = np.array([  [Sigm11, Sigm12],
-                    [Sigm12, Sigm22]])
+                    [Sigm12, Sigm22]], dtype=complex)
+    # print("S", np.array(S))
+
     normalization = 2*np.pi*np.sqrt(np.linalg.det(S))
     S_inv = stupid_inverse(S)
-    result = np.zeros((N, M))
+    result = np.zeros((N, M), dtype=complex)
     for i in range(N):
       for j in range(M):
         extended_vect = np.array([X[i,j], V[i,j]])
         extended_init = np.array([x0, v0])
         mu = extended_vect - np.dot(G, extended_init)
-        exponent = -0.5*np.dot(mu, np.dot(S_inv, mu.time_index))
+        exponent = -0.5*np.dot(mu, np.dot(S_inv, mu.T))
         result[i, j] = np.exp(exponent)/normalization
         
-    cdef double norm = 0.0
-    for i in range(N):
-      for j in range(M):
-        norm += result[i,j]
-    print(f"norm of analytic is {norm*(X[0,1] - X[0, 0])*(V[1, 0] - V[0,0])}")
     return result
+
+cpdef quad_int(double [:,:] f, double [:] x, double [:] y):
+    cdef int N = f.shape[0], M = f.shape[1]
+    cdef float dx = x[1] - x[0], dy = y[1] - y[0]
+    cdef double sum = 0.0
+    cdef double [:] intermediate = np.zeros(N)
+    cdef int i = 0, j = 0
+
+    for i in range(N):
+        for j in range(M-1):
+            intermediate[i] += f[i, j] + f[i, j+1]
+        intermediate[i] *= 0.5 * dy
+    
+    for i in range(N-1):
+        sum += intermediate[i] + intermediate[i+1]
+    sum *= 0.5 * dx
+
+    return sum
+
+
+
+
+
+
+    
+
